9a0df9ff3e989294f7d46136a74cfc68ea0acd8d
lee <tkdrb4807@gmail.com>
2020-06-19 01:31:48 +0900

    디테일 작업을 위한 인턴페이스 생성

---
+++ app/src/main/java/com/sama/communicationclassjava/Contract/GalleryDetailCommentKeyAdpterContract.java
@@ -0,0 +1,5 @@
+package com.sama.communicationclassjava.Contract;
+
+public interface GalleryDetailCommentKeyAdpterContract {
+    Im
+}
---
+++ app/src/main/java/com/sama/communicationclassjava/Contract/GalleryDetailImageAdpterContract.java
@@ -0,0 +1,4 @@
+package com.sama.communicationclassjava.Contract;
+
+public interface GalleryDetailImageAdpterContract {
+}
