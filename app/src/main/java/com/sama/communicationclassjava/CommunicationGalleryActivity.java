955d355deb98a2dad837427480e51148771c07e7
lee <tkdrb4807@gmail.com>
2020-06-22 02:13:46 +0900

    상세화면 진입 코드 작성

--- app/src/main/java/com/sama/communicationclassjava/CommunicationGalleryActivity.java
+++ app/src/main/java/com/sama/communicationclassjava/CommunicationGalleryActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.app.ProgressDialog;
@@ -17,13 +16,13 @@ import android.widget.Toast;
 import com.sama.communicationclassjava.Adapter.GalleryAdapter;
 import com.sama.communicationclassjava.Contract.GalleryContract;
 import com.sama.communicationclassjava.CustomActionBar.CustomAction;
-import com.sama.communicationclassjava.Data.CommunicationItem;
-import com.sama.communicationclassjava.Data.GalleryDatilData;
+import com.sama.communicationclassjava.Data.GalleryDetailData;
 import com.sama.communicationclassjava.Lisetner.OnItemClickListener;
 import com.sama.communicationclassjava.Presenter.GalleryPresenter;
 
 public class CommunicationGalleryActivity extends AppCompatActivity
         implements GalleryContract.View, View.OnClickListener, OnItemClickListener {
+
     private GalleryContract.Presenter presenter;
     private GalleryAdapter galleryAdapter;
     private RecyclerView SubjectRecyclerView;
@@ -38,38 +37,26 @@ public class CommunicationGalleryActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_communication_gallery);
 
-
         CustomAction customAction = new CustomAction(getSupportActionBar(), this);
         customAction.setActionBar();
         getSupportActionBar().setCustomView(R.layout.custom_toolbarlayout);
 
-
         GalleryaddButten = findViewById(R.id.Gallery_add_layout);
         SubjectRecyclerView = (RecyclerView) findViewById(R.id.CommunicationGalleryRecyclerView);
-
-
 
         GalleryaddButten.setOnClickListener(this);
 
         this.galleryAdapter = new GalleryAdapter();
-
         this.presenter = new GalleryPresenter();
         this.presenter.attachView(this);
-
-
-
         this.presenter.setOptionAdapterModel(galleryAdapter);
         this.presenter.setOptionAdapterView(galleryAdapter);
         this.galleryAdapter.setOnClickLisetner(this);
 
 
         this.manager = new GridLayoutManager(this, 2  );
-
         this.SubjectRecyclerView.setLayoutManager(manager);
         SubjectRecyclerView.setAdapter(galleryAdapter);
-
-
-
 
         SubjectRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
@@ -91,8 +78,8 @@ public class CommunicationGalleryActivity extends AppCompatActivity
         this.mProgressDialog.setProgress(ProgressDialog.STYLE_SPINNER);
         this.mProgressDialog.setProgressStyle(android.R.style.Widget_ProgressBar_Horizontal);
         this.mProgressDialog.setMessage("로딩중");
-//        this.mProgressDialog.setCancelable(false);
-        presenter.ContentsLoading();
+        this.mProgressDialog.setCancelable(false);
+        this.presenter.ContentsLoading();
 
     }
 
@@ -104,43 +91,34 @@ public class CommunicationGalleryActivity extends AppCompatActivity
     }
 
     @Override
-    public void TestView() {
-
+    public void changeContentsTypeChoiceActivity() {
+        Intent  Intent = new Intent(this, ContentsTypeChoiceActivity.class);
+        startActivity(Intent);
     }
 
     @Override
-    public void Histortview() {
-
+    public void changeDetailActivity(GalleryDetailData datilData) {
+        Intent  Intent = new Intent(this, CommunicationDetailActivity.class);
+        startActivity(Intent);
     }
 
-    @Override
-    public void DetailActivity(CommunicationItem item) {
-        Toast.makeText(this, item.toString(), Toast.LENGTH_SHORT).show();
-//        Intent myIntent = new Intent(this, CommunicationDetailActivity.class);
-//        startActivity(myIntent);
-    }
 
     @Override
     public void onClick(View v) {
-        Intent myIntent;
+
         switch (v.getId()){
             case R.id.Gallery_add_layout :
-                myIntent = new Intent(this, ContentsTypeChoiceActivity.class);
-                startActivity(myIntent);
-                break;
-            default:
+                presenter.changeContentsTypeChoiceActivity();
                 break;
         }
-
-
     }
 
     @Override
-    public void onItemClickListener(int position, GalleryDatilData item) {
-//        Intent myIntent = new Intent(this, CommunicationDetailActivity.class);
-//        startActivity(myIntent);
+    public void onItemClickListener(int position, GalleryDetailData item) {
+        Intent intent = new Intent(this, CommunicationDetailActivity.class);
+        intent.putExtra("DetailData", item);
+        startActivity(intent);
     }
-
 
     @Override
     public void ItemloadingAlertShow() {
--- app/src/main/java/com/sama/communicationclassjava/Presenter/GalleryPresenter.java
+++ app/src/main/java/com/sama/communicationclassjava/Presenter/GalleryPresenter.java
@@ -4,7 +4,7 @@ package com.sama.communicationclassjava.Presenter;
 import com.sama.communicationclassjava.Contract.GalleryContract;
 import com.sama.communicationclassjava.Contract.GalleryAdapterContract;
 import com.sama.communicationclassjava.Data.CommunicationItem;
-import com.sama.communicationclassjava.Data.GalleryDatilData;
+import com.sama.communicationclassjava.Data.GalleryDetailData;
 import com.sama.communicationclassjava.Lisetner.OnGallerySelectItemListListener;
 import com.sama.communicationclassjava.Model.FireBaseModel;
 
@@ -14,14 +14,14 @@ import java.util.Date;
 public class GalleryPresenter implements GalleryContract.Presenter ,OnGallerySelectItemListListener{
 
     GalleryContract.View view;
-    ArrayList<CommunicationItem> communicationItemArrayList;
     GalleryAdapterContract.Model adapterModel;
     GalleryAdapterContract.View adapterView;
     FireBaseModel fireBaseModel = FireBaseModel.getInstance();
+
+
     public GalleryPresenter() {
         super();
-        fireBaseModel.setSelectItemListListener(this);
-
+        fireBaseModel.setOnSelectGalleryListener(this);
     }
 
     @Override
@@ -34,29 +34,33 @@ public class GalleryPresenter implements GalleryContract.Presenter ,OnGallerySel
         this.view = null;
     }
 
+
+
     @Override
-    public void notfyAdaoter() {
-        this.adapterView.notfyAdaoter();
+    public void changeDetailActivity(GalleryDetailData item) {
+        this.view.changeDetailActivity(item);
     }
 
-    @Override
-    public void DetailActivity(CommunicationItem item) {
 
+
+    @Override
+    public void notfyAdapter() {
+        this.adapterView.notfyAdaoter();
     }
 
     @Override
     public void setOptionAdapterModel(GalleryAdapterContract.Model Model) {
         this.adapterModel = Model;
-//        this.communicationItemArrayList = new ArrayList<>();
-//        this.communicationItemArrayList.add(new CommunicationItem("1","1","1","1"));
-//        this.communicationItemArrayList.add(new CommunicationItem("2","2","2","2"));
-//        this.communicationItemArrayList.add(new CommunicationItem("3","3","3","3"));
-//        this.adapterModel.addItems(this.communicationItemArrayList);
     }
 
     @Override
     public void setOptionAdapterView(GalleryAdapterContract.View View) {
         this.adapterView = View;
+    }
+
+    @Override
+    public void changeContentsTypeChoiceActivity() {
+        this.view.changeContentsTypeChoiceActivity();
     }
 
 
@@ -71,10 +75,10 @@ public class GalleryPresenter implements GalleryContract.Presenter ,OnGallerySel
     }
 
     @Override
-    public void OnGallerySelectItemList(ArrayList<GalleryDatilData> list) {
+    public void OnGallerySelectItemList(ArrayList<GalleryDetailData> list) {
         view.ItemloadingAlertDisabled();
         this.adapterModel.addItems(list);
-        this.notfyAdaoter();
+        this.notfyAdapter();
     }
 }
 
